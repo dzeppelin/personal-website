4f77b05a0d78dc45f7fb6ab643829d67db33730f
Abulkhair <abzh423@protonmail.com>
2022-01-03 16:34:26 +0600

    Completed Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import '../styles/navbar.css';
 
 const Navbar: FC = () => {
+    // current page from useLocation
+    const { pathname } = useLocation();
+    const splitLocation = pathname.split('/');
+
+    // makes text-black if the page is active
+    const thePath = (path: string): string => {
+        return splitLocation[1] === path ? 'text-black' : '';
+    };
+
     return (
         <nav className="flex flex-row justify-between">
-            <span className="logo">talapov</span>
-            <ul className="flex flex-row px-10 last:pl-10 first:pr-10">
+            <span className="logo text-4xl pb-2">
+                <Link to="/">talapov</Link>
+            </span>
+            <ul className="flex flex-row items-center text-gray-500">
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/" className={thePath('')}>
+                        Home
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/blog">Blog</Link>
+                    <Link to="/blog" className={thePath('blog')}>
+                        Blog
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/portfolio">Portfolio</Link>
+                    <Link to="/portfolio" className={thePath('portfolio')}>
+                        Portfolio
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/contacts">Contacts</Link>
+                    <Link to="/contacts" className={thePath('contacts')}>
+                        Contacts
+                    </Link>
                 </li>
             </ul>
         </nav>
