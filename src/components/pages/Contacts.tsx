1898cc0e8a731c4dca37501ebe1e9a561abbda6d
Abulkhair <abzh423@protonmail.com>
2022-01-24 21:51:37 +0600

    Some changes to contacts section

--- src/components/pages/Contacts.tsx
+++ src/components/pages/Contacts.tsx
@@ -6,14 +6,27 @@ const Contacts = () => {
 		<>
 			{/* Credentials */}
 			<div>
-				<h2 className='text-xl sm:text-2xl font-bold tracking-tight mt-10 mb-4'>
-					Contacts
+				<h2 className='text-xl sm:text-2xl font-bold tracking-tight mb-4'>
+					Contact Me
 				</h2>
-				<ul className='flex flex-col contacts'>
-					<li>My Blog</li>
-					<li>Github</li>
-					<li>Email</li>
-				</ul>
+				<div className='ring-1 ring-black p-12 rounded-lg shadow-lg flex flex-row'>
+					<div className='form flex flex-col'>
+						<input
+							type='text'
+							name=''
+							id=''
+							className='ring-1 rounded-lg ring-gray-500'
+						/>
+						<button>Send</button>
+					</div>
+					<div className='contacts'>
+						<ul className='flex flex-col justify-between contacts'>
+							<li>My Blog</li>
+							<li>Github</li>
+							<li>Email</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</>
 	);
