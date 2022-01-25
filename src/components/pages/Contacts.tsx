37b8e60b1fef4eef7cd01bac94209123d6d5056b
Abulkhair <abzh423@protonmail.com>
2022-01-25 20:31:34 +0600

    Added fields for fast email form

--- src/components/pages/Contacts.tsx
+++ src/components/pages/Contacts.tsx
@@ -10,17 +10,25 @@ const Contacts = () => {
 					Contact Me
 				</h2>
 				<div className='ring-1 ring-black p-12 rounded-lg shadow-lg flex flex-row'>
-					<div className='form flex flex-col'>
+					<div className='form flex flex-col mr-20'>
 						<input
 							type='text'
 							name=''
 							id=''
-							className='ring-1 rounded-lg ring-gray-500'
+							className='ring-1 rounded-lg ring-gray-500 mb-3 p-1 px-2'
 						/>
-						<button>Send</button>
+						<input
+							type='text'
+							name=''
+							id=''
+							className='ring-1 rounded-lg ring-gray-500 p-1 px-2 h-20'
+						/>
+						<div className='flex flex-row justify-items-start'>
+							<button>Send</button>
+						</div>
 					</div>
 					<div className='contacts'>
-						<ul className='flex flex-col justify-between contacts'>
+						<ul className='flex flex-col contacts'>
 							<li>My Blog</li>
 							<li>Github</li>
 							<li>Email</li>
