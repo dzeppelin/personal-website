34ce9ee6fd1dfc0460631a33f55feefe801a35cb
Abulkhair <abzh423@protonmail.com>
2022-01-30 22:33:54 +0600

    More changes to ui of contacts page

--- src/components/pages/Contacts.tsx
+++ src/components/pages/Contacts.tsx
@@ -7,47 +7,50 @@ const Contacts = () => {
 			{/* Credentials */}
 			<div>
 				<h2 className='text-xl sm:text-2xl font-bold tracking-tight mb-4'>
-					Contact Me
+					Fast Email
 				</h2>
 
-				<div className='flex flex-row w-full ring-1 ring-black rounded-lg p-16'>
-					<div className='form flex flex-col w-3/4'>
+				<div className='w-full ring-1 ring-black rounded-lg p-12 shadow-lg'>
+					<div className='form flex flex-col w-full'>
 						<p className='mb-2 font-medium'>Header</p>
 						<input
 							type='text'
 							name=''
 							id=''
-							className='ring-1 rounded-lg ring-gray-500 mb-2 p-1 px-2 w-full'
+							className='ring-1 rounded-lg ring-gray-500 mb-4 p-1 px-2 w-full'
 						/>
 						<p className='mb-2 font-medium'>Body</p>
-						<input
-							type='text'
+						<textarea
 							name=''
 							id=''
-							className='ring-1 rounded-lg ring-gray-500 p-1 px-2 pb-20 w-full'
-						/>
+							rows={4}
+							className='ring-1 ring-black rounded-lg p-1 px-2'
+						></textarea>
 						<div className='flex flex-row justify-items-start mt-4'>
 							<button className='ring-2 ring-black px-5 py-1 rounded-full hover:shadow-lg'>
 								Send
 							</button>
 						</div>
 					</div>
-					<div className='contacts w-1/4 flex flex-col mt-2'>
-						<ul className='flex flex-col contacts items-center contact-links ml-10'>
-							<li>
-								<a href=''>Blog</a>
-								<img src='hashnode.png' alt='hashnode logo' />
-							</li>
-							<li>
-								<a href=''>Github</a>
-								<img src='github.png' alt='github logo' />
-							</li>
-							<li>
-								<a href=''>Email</a>
-								<img src='email.jpg' alt='email icon' />
-							</li>
-						</ul>
-					</div>
+				</div>
+				<h2 className='text-xl sm:text-2xl font-bold tracking-tight mb-4 mt-10'>
+					Links
+				</h2>
+				<div className='w-1/4 flex flex-col mt-2'>
+					<ul className='flex flex-row contacts items-center contacts'>
+						<li>
+							<a href=''>Blog</a>
+							<img src='hashnode.png' alt='hashnode logo' />
+						</li>
+						<li>
+							<a href=''>Github</a>
+							<img src='github.png' alt='github logo' />
+						</li>
+						<li>
+							<a href=''>Email</a>
+							<img src='email.jpg' alt='email icon' />
+						</li>
+					</ul>
 				</div>
 			</div>
 		</>
