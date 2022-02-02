545fcf1bc701bb931249c237589fe033e4ed26e5
Abulkhair <abzh423@protonmail.com>
2022-02-02 15:00:41 +0600

    Refactoring

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from 'components/layout/Layout';
+import Layout from 'components/Layout';
 
 /* Website pages */
 import Home from 'components/pages/Home';
